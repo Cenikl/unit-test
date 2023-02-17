2b3f2f1dc268aec3924ecca4f4f344ad581943ce
Cedric <99885260+Cenikl@users.noreply.github.com>
2023-02-17 12:53:49 +0300

    chore: finishing TODO

--- src/tests/ListItem.test.jsx
+++ src/tests/ListItem.test.jsx
@@ -1,5 +1,5 @@
-import { render } from "@testing-library/react";
-
+import { render, fireEvent } from "@testing-library/react";
+import renderer from 'react-test-renderer';
 import { ListItem } from "../ListItem";
 
 const mockOnCheck = jest.fn();
@@ -28,9 +28,10 @@ describe('ListItem', () => {
                 item='Lorem ipsum dolor sit amet consectetur'
             />
         );
-
+        const node = getByTestId('test-list-item-1-container');
         const value = getByTestId('test-list-item-1');
         expect(value).toBeInTheDocument();
+        expect(node.children).toHaveLength(2);
     });
 
     it('checkbox is hidden', () => {
@@ -47,21 +48,59 @@ describe('ListItem', () => {
     });
     
     //TODO: implement this
-    it('callback is called', () => {});
+    it('callback is called', () => {
+        const { getByTestId } = render(
+            <ListItem
+                id='list-item-1'
+                checkable={true}
+                onCheck={mockOnCheck}
+                item='Lorem ipsum dolor sit amet consectetur'
+            />
+        );
+        const checking = getByTestId('test-list-item-1');
+        fireEvent.click(checking);
+        expect(mockOnCheck).toBeCalledTimes(1);
+    });
 
     //TODO: implement this
-    it('callback is not called when not checkable', () => {});
+    it('callback is not called when not checkable', () => {
+        const { getByTestId } = render(
+            <ListItem
+                id='list-item-1'
+                checkable={false}
+                onCheck={mockOnCheck}
+                item='Lorem ipsum dolor sit amet consectetur'
+            />
+        );
+        const node = getByTestId('test-list-item-1-container');
+        
+        expect(node.children).toHaveLength(1);
+        expect(mockOnCheck).toBeCalledTimes(0);
+    });
 
     //TODO: implement this
     it('matches saved snapshot', () => {
-        const tree = render(
+        const tree = renderer.create(
             <ListItem
                 id='list-item-test'
                 checkable={true}
                 onCheck={mockOnCheck}
                 item='Lorem ipsum dolor sit amet consectetur'
             />
-        );
+        ).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+    
+    it('matches saved snapshot 2', () => {
+        const tree = renderer.create(
+            <ListItem
+                id='list-item-test'
+                checkable={false}
+                onCheck={mockOnCheck}
+                item='Lorem ipsum dolor sit amet consectetur'
+            />
+        ).toJSON();
         expect(tree).toMatchSnapshot();
     });
 });
